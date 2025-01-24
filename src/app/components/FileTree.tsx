697e4194158ed0bb699d36c2ec12bd7c3721e355
darren-zhang <file-mixer@example.com>
2025-01-25 00:14:25 +0800

    改英文

--- src/app/components/FileTree.tsx
+++ src/app/components/FileTree.tsx
@@ -6,7 +6,7 @@ import { FaFile, FaTimes } from 'react-icons/fa';
 type FileNode = {
   name: string;
   type: 'file' | 'directory';
-  children?: FileNode[];
+  children?: FileNode[]; 
   path: string;
 };
 
